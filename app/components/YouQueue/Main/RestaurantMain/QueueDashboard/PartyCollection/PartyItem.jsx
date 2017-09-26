56b2eab47e70f569cafc466e04ffc3d173a2af6d
John Silkey <jpsilkjr86@gmail.com>
2017-09-25 18:23:44 -0700

    added dynamic rendering to buttons based on party data values for PartyItem

--- app/components/YouQueue/Main/RestaurantMain/QueueDashboard/PartyCollection/PartyItem.jsx
+++ app/components/YouQueue/Main/RestaurantMain/QueueDashboard/PartyCollection/PartyItem.jsx
@@ -2,12 +2,12 @@
 import React from 'react';
 
 // declares PartyItem as pure functional component (stateless)
-const PartyItem = ({party}) => (
+const PartyItem = ({party, alerted_sms, arrived_table}) => (
 
 	<li className="collection-item dismissable party-row" data-id="">
 		<div className="row">
 		  <div className="col s3 m2">
-		    <a className="btn-floating sms-btn" data-id="" data-alerted_sms=""><i className="material-icons">phonelink_ring</i></a>
+		    <SMSButton alerted_sms={alerted_sms}/>
 		  </div>
 		  <div className="col s6 m6">
 		    <p>Party: <b>{party.party_name}</b></p>
@@ -17,17 +17,49 @@ const PartyItem = ({party}) => (
 		  <div className="col s3 m4">
 		    <div className="row">
 		      <div className="col s12 m6">
-		        <a className="btn-floating cancel-btn red" data-id="" data-active=""><i className="material-icons">close</i></a>
+		        <CancelButton/>
 		      </div>
 		      <div className="col s12 m6">
-		        <a className="btn-floating arrived-btn green" data-id="" data-arrived_table=""><i className="material-icons">check_box_outline_blank</i></a>
+		        <ArrivedTableButton arrived_table={arrived_table}/>
 		      </div>
 		    </div>
 		  </div>                                    
 		</div>
 	</li>
 
-)
+);
+
+// booleans in Party model:  is_active, arrived_table, alerted_sms
+
+const SMSButton = ({alerted_sms}) => (
+	alerted_sms ? (
+		<a className="btn-floating sms-btn" data-id="" data-alerted_sms="">
+			<i className="material-icons">phonelink_ring</i>
+		</a>
+	) : (
+		<a className="btn-floating sms-btn green" data-id="" data-alerted_sms="">
+			<i className="material-icons">phonelink_ring</i>
+		</a>
+	)
+);
+
+const CancelButton = props => (
+	<a className="btn-floating cancel-btn red" data-id="" data-active="">
+		<i className="material-icons">close</i>
+	</a>
+);
+
+const ArrivedTableButton = ({arrived_table}) => (
+	arrived_table ? (
+		<a className="btn-floating arrived-btn green" data-id="" data-arrived_table="">
+			<i className="material-icons">check_box_outline_blank</i>
+		</a>
+	) : (
+		<a className="btn-floating arrived-btn red" data-id="" data-arrived_table="">
+			<i className="material-icons">check_box_outline_blank</i>
+		</a>
+	)
+);
 
 // exports PartyItem component for other files to use
 export default PartyItem;
