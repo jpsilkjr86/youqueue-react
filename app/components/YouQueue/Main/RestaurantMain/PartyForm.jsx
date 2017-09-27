448987195fc223b44135f1126634affe5ef9d221
Tarmin Sidharta <tsidharta@gmail.com>
2017-09-26 22:07:30 -0700

    working on Party Form

--- app/components/YouQueue/Main/RestaurantMain/PartyForm.jsx
+++ app/components/YouQueue/Main/RestaurantMain/PartyForm.jsx
@@ -11,63 +11,92 @@ class PartyForm extends Component {
 	constructor(props) {
 		super(props);
 
-		// set initial state
+		// set initial state 
 	  this.state = { 
-			
+			party_name: "",
+			party_size: "",
+			phone_number: "",
+			email: "",
+			first_name: "",
+			last_name: ""
 		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmitForm = this.handleSubmitForm.bind(this);
+
 	} 
+
+	handleChange(event) {
+		
+    const { value, name } = event.target;
+    
+    // sets state of whatever the input name is to the value of event object
+    this.setState({
+      [name]: value
+    });
+
+	}
+
+	handleSubmitForm(event) {
+		console.log(this.state);
+		// prevents default form behavior
+    event.preventDefault();
+
+    const party = this.state;
+    // clears state data, triggering re-rendering of component to empty form
+    this.setState({
+      party_name: "",
+			party_size: "",
+			phone_number: "",
+			email: "",
+			first_name: "",
+			last_name: ""
+    });
+    // sends query and results to parent through inherited function onSearch()
+    this.props.onSearch(party);
+
+	}
 
   render() {
   	const { restaurant_id } = this.props;
   	return (
   		<FormContainer>
-				<form id="new-customer" action="/customer/add" method="post">			                
+				<form id="new-customer" onSubmit={this.handleSubmitForm}>			                
 					<div className="row">
 					  <h5 className="center-align">Add Customer</h5>
 					  <p className="right"><em>*Required input</em></p>
 			      <div className="row" id="partyform-row-wrapper">
 			        <div className="input-field col s12 m6">
 			          <i className="material-icons prefix">group</i>
-			          <input id="party_name" type="text" className="validate" required="required" name="party_name"/>
+			          <input id="party_name" type="text" className="validate" value={this.state.party_name} onChange={this.handleChange} required="required" name="party_name"/>
 			          <label htmlFor="party_name">*Party Name</label>
 			        </div>
 			      	<div className="input-field col s12 m6">
 			        	<i className="material-icons prefix">group</i>
-			          <input id="party_size" type="number" className="validate" required="required" name="party_size"/>
+			          <input id="party_size" type="number" className="validate" value={this.state.party_size} onChange={this.handleChange} required="required" name="party_size"/>
 			          <label htmlFor="party_size">*Party Size</label>
 			        </div>
 			        <div className="input-field col s12 m6">
 			          <i className="material-icons prefix">phone</i>
-			          <input id="phone_number" type="tel" className="validate" required="required" name="phone_number"/>
+			          <input id="phone_number" type="tel" className="validate" value={this.state.phone_number} onChange={this.handleChange} required="required" name="phone_number"/>
 			          <label htmlFor="phone_number">*Telephone</label>
 			        </div>
 			        <div className="input-field col s12 m6">
 			          <i className="material-icons prefix">email</i>
-			          <input id="email-new" type="email" className="validate" required="required" name="email"/>
+			          <input id="email-new" type="email" className="validate" value={this.state.email} onChange={this.handleChange} required="required" name="email"/>
 			          <label htmlFor="email">Email</label>
 			        </div>
 			        <div className="input-field col s12 m6">
 			          <i className="material-icons prefix">account_circle</i>
-			          <input id="first_name" type="text" className="validate" required="required" name="first_name"/>
+			          <input id="first_name" type="text" className="validate" value={this.state.first_name} onChange={this.handleChange} required="required" name="first_name"/>
 			          <label htmlFor="first_name">*First Name</label>
 			        </div>
 			        <div className="input-field col s12 m6">
 			          <i className="material-icons prefix">account_circle</i>
-			          <input id="last_name" type="text" className="validate" name="last_name"/>
+			          <input id="last_name" type="text" className="validate" value={this.state.last_name} onChange={this.handleChange} name="last_name"/>
 			          <label htmlFor="last_name">Last Name</label>
 			        </div>
-			        <div className="input-field col s12">
-			          <i className="material-icons prefix">event</i>
-			          <select multiple name="occasion" form="new-customer" required>
-			            <option value="" disabled selected>*Choose one or more occasion.</option>
-			            <option value="casual dining">Casual Dining</option>
-			            <option value="birthday">Birthday</option>
-			            <option value="wedding anniversary">Wedding Anniversary</option>
-			            <option value="family gathering">Family Gathering</option>
-			            <option value="other">Other</option>
-			          </select>
-			          <label htmlFor="occasion">*Occasion</label>
-			        </div>
+			        
 			        <div className="col s12 center">
 			          <Link to="/" className="btn btn-large waves-effect waves-light red lighten-2 left">Cancel</Link>
 			          <button className="btn btn-large waves-effect waves-light right" type="submit">Submit</button>
