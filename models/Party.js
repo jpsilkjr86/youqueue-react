ab6c17fd7c17479f7d8cd6649e834429b07648c9
John Silkey <jpsilkjr86@gmail.com>
2017-10-05 23:28:48 -0700

    changed restaurant_id from String to ObjectId with ref to RestaurantUser model

--- models/Party.js
+++ models/Party.js
@@ -15,11 +15,9 @@ const PartySchema = new Schema({
   arrived_table: { type: Boolean, default: false },
   alerted_sms: { type: Boolean, default: false },
   entered_queue_at: { type: Date, default: Date.now },
-  // for testing only - delete later
-  restaurant_id: {  type: String, required: true },
-  // // references Restaurant model
-  // restaurant_id: { type: Schema.Types.ObjectId, ref: "Restaurant" },
-  // // refers to a returning Customer
+  // // references RestaurantUser model
+  restaurant_id: { type: Schema.Types.ObjectId, ref: "RestaurantUser" },
+  // // refers to a returning Customer (later versions may have this; not currently available)
   // customer_id: { type: Schema.Types.ObjectId, ref: "Customer" }
 });
 
