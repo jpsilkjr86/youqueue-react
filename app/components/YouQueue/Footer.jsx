50b60079c9f84557b772e2371b3552a5116d6ad8
John Silkey <jpsilkjr86@gmail.com>
2017-11-24 14:53:55 -0700

    component-alized LinkedInLink's in footer. added tyler's linked-in link (was missing before)

--- app/components/YouQueue/Footer.jsx
+++ app/components/YouQueue/Footer.jsx
@@ -1,6 +1,17 @@
 // imports react
 import React from 'react';
 
+ const 	michaelLink = "https://www.linkedin.com/in/michael-tolmie-83527537/",
+ 				johnLink = "https://www.linkedin.com/in/john-silkey-jr/",
+ 				tarminLink = "https://www.linkedin.com/in/tarmin-sidharta/",
+ 				tylerLink = "https://www.linkedin.com/in/tyler-r-peterson/";
+
+const LinkedInLink = ({ href, text }) => (
+	<a className="grey-text text-lighten-3" target="_blank" href={href}>
+		{text}
+	</a>
+);
+
 // declares Footer pure functional component, which will be this file's export
 const Footer = props => (
 	<footer className="page-footer light-blue darken-4">
@@ -9,9 +20,10 @@ const Footer = props => (
 				<div className="col s12 m6 white-text">
 					<h5>Connect with the contributors:</h5>
 					<ul>
-						<li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/michael-tolmie-83527537/">Michael</a></li>
-						<li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/john-silkey-jr/">John</a></li>
-						<li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/tarmin-sidharta/">Tarmin</a></li>
+						<li><LinkedInLink href={michaelLink} text="Michael"/></li>
+						<li><LinkedInLink href={johnLink} text="John"/></li>
+						<li><LinkedInLink href={tarminLink} text="Tarmin"/></li>
+						<li><LinkedInLink href={tylerLink} text="Tyler"/></li>
 					</ul> 
 				</div>
 				<div className="col s12 m3 offset-m3 white-text">
