9f8e3169e40a55392a57ef5af87ca4725a0b675a
John Silkey <jpsilkjr86@gmail.com>
2017-11-28 17:48:12 -0700

    added phone number front-end validation for SignUpModal (same as on PartyForm, with auto-completion of hyphens and regex pattern recognition

--- app/components/YouQueue/Login/SignUpModal.jsx
+++ app/components/YouQueue/Login/SignUpModal.jsx
@@ -21,6 +21,7 @@ class SignUpModal extends Component {
 		};
 
 		this.handleChange = this.handleChange.bind(this);
+		this.handlePhoneChange = this.handlePhoneChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 
 	}
@@ -29,6 +30,7 @@ class SignUpModal extends Component {
 		$('#signup-modal').modal('close')
 	}
 
+	// general handler for input value changes
 	handleChange(event) {
 		
     const { value, name } = event.target;
@@ -37,6 +39,40 @@ class SignUpModal extends Component {
       [name]: value
     });
 
+	}
+
+	// specifically handles phone number input changes
+	handlePhoneChange(event) {
+		// console.log(event.target);
+		const { value } = event.target;
+
+		// if the input value has 3 characters and consists of 3 digits, add hyphen
+		if (value.length === 3 && /\d{3}/.test(value)) {
+			this.setState({
+	      phone_number: value + '-'
+	    });
+		}
+		// if input value has 7 characters and is in 123-456 format, add hyphen
+		else if (value.length === 7 && /\d{3}-\d{3}/.test(value)) {
+			this.setState({
+	      phone_number: value + '-'
+	    });
+		}
+		// tests to see if phone number input format is correct and complete
+		else if (value.length === 12 && /\d{3}-\d{3}-\d{4}/.test(value)) {
+			this.setState({
+	      phone_number: value
+	    }, () => {
+	    	// focuses on next input automatically after correct format is reached
+	    	this.inputAfterPhoneNumber.focus();
+	    });
+		}
+		// default is just update state with input value
+		else {
+			this.setState({
+	      phone_number: value
+	    });
+		}
 	}
 
 	handleSubmit(event) {
@@ -111,8 +147,13 @@ class SignUpModal extends Component {
 		          	className="validate"
 		          	required="required"
 		          	name="phone_number"
-		          	onChange={this.handleChange}
+		          	onChange={this.handlePhoneChange}
 		          	value={this.state.phone_number}
+		          	// pattern ensures phone number is in proper format (123-456-7890)
+		          	pattern="\d{3}-\d{3}-\d{4}"
+		          	// title appears on hover and on incorrect input submission
+		          	title="123-456-7890"
+		          	maxLength="12"
 		          />
 		          <label htmlFor="phone_number">*Telephone</label>
 		        </div>
@@ -125,6 +166,8 @@ class SignUpModal extends Component {
 		          	name="email"
 		          	onChange={this.handleChange}
 		          	value={this.state.email}
+		          	// grabbing ref for auto-focusing after phone number format is complete
+		          	ref={inp => this.inputAfterPhoneNumber = inp}
 		          />
 		          <label htmlFor="email">*Email</label>
 		        </div>
